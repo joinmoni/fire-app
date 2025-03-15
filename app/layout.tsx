75c1153de17028f10e05849a73f7513ca72b1c7b
Adebola Adeniran <onedebos@gmail.com>
2025-03-15 14:48:25 +0000

    update preview icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,12 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Moni FIRE ",
+  title: "Moni FIRE",
   description: "Achieve Financial Independence and Retire Early",
+  icons: {
+    icon: "/moni-icon.svg",
+    apple: "/moni-icon.svg",
+  },
 };
 
 export default function RootLayout({
