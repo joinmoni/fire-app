4eede6eab5788c699d22807f51f3603187a526cf
Adebola Adeniran <onedebos@gmail.com>
2025-03-15 15:11:13 +0000

    update description

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,18 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "FIRE with Moni",
-  description: "Achieve Financial Independence and Retire Early",
+  title: "Moni FIRE",
+  description:
+    "Calculate your path to Financial Independence and Retire Early (FIRE). Discover when you can achieve FIRE, track your progress, and create a personalized roadmap to financial freedom. Our tools help you determine your FIRE age, optimize your savings rate, and plan your investment strategy for a secure early retirement.",
   icons: {
     icon: "/favicon.ico",
     apple: "/favicon.ico",
   },
+  metadataBase: new URL("https://moni.africa"),
   openGraph: {
+    title: "FIRE with Moni",
+    description:
+      "Calculate your path to Financial Independence and Retire Early (FIRE). Discover when you can achieve FIRE, track your progress, and create a personalized roadmap to financial freedom.",
     images: [
       {
         url: "/moni-icon.svg",
@@ -17,6 +22,7 @@ export const metadata: Metadata = {
         alt: "Moni FIRE Logo",
       },
     ],
+    type: "website",
   },
   twitter: {
     card: "summary",
